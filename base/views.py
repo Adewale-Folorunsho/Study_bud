d842f0e813246b94b553b5596a35aa7db91875d2
Adewale-Folorunsho <walefolorunsho45@gmail.com>
2023-07-14 18:22:40 -0500

    created form to create room

--- base/views.py
+++ base/views.py
@@ -9,10 +9,15 @@ rooms = [
 
 def home(request):
     rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(Request):
+
+    context = {}
+    return render(Request, 'base/room_form.html', context)
