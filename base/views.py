43dddae2c8e5d5c5c80d796376c102dcb7807b0d
Adewale-Folorunsho <walefolorunsho45@gmail.com>
2023-07-13 19:18:19 -0500

    created 'room' DB and fetched data from DB

--- base/views.py
+++ base/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import Room
+
+rooms = [
+    {'id':1, 'name':'Python'},
+    {'id':2, 'name':'React'},
+    {'id':3, 'name':'Django'},
+]
 
 def home(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
