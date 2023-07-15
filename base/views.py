d9e89d8377f926a0768c0c2e86616c1a2166d6a8
Adewale-Folorunsho <walefolorunsho45@gmail.com>
2023-07-14 20:55:57 -0500

    user can filter and see no of rooms

--- base/views.py
+++ base/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from .models import Room, Topic
 from .forms import RoomForm
-
+from django.db.models import Q
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q))
+    room_count = rooms.count()
     topics = Topic.objects.all()
-    context = {'rooms': rooms, 'topics':topics}
+    context = {'rooms': rooms, 'topics':topics, 'room_count':room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
